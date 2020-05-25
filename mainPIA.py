f236bc05c02be9ad6295cf18a5cba763d922dce5
Alexa Sherlyn Camacho Ayala <alexasherlyn18@gmail.com>
2020-05-25 18:24:12 -0500

    Estructura inicial del main

---
+++ mainPIA.py
@@ -0,0 +1,22 @@
+print("***** MENU INICIAL *****")
+print("1.- Empleados\n2.- Cursos\n3.- Temas\n4.- Videos\n5.- Temas asignados al curso\n6.- Videos asignados a un tema")
+
+opcionInicial = int(input("Elija una opcion: "))
+
+if opcionInicial == 1:
+    print("***** EMPLEADOS *****")
+
+if opcionInicial == 2:
+    print("***** CURSOS *****")
+
+if opcionInicial == 3:
+    print("***** TEMAS *****")
+
+if opcionInicial == 4:
+    print("***** VIDEOS *****")
+
+if opcionInicial == 5:
+    print("***** TEMAS ASIGNADOS AL CURSO *****")
+
+if opcionInicial == 6:
+    print("***** VIDEOS ASIGNADOS A UN TEMA *****")
