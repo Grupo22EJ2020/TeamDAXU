da374163e7af1a7dda20b1d8d29dc3368b7a50fd
Alexa Sherlyn Camacho Ayala <alexasherlyn18@gmail.com>
2020-05-25 18:45:00 -0500

    Estructura main casi completa

--- mainPIA.py
+++ mainPIA.py
@@ -3,27 +3,98 @@ print("1.- Empleados\n2.- Cursos\n3.- Temas\n4.- Videos\n5.- Temas asignados al 
 
 opcionInicial = int(input("Elija una opcion: "))
 
-
 if opcionInicial == 1:
     print("\n***** EMPLEADOS *****")
     print("1.- Agregar\n2.- Borrar\n3.- Modificar\n4.- Consultar todo\n5.- Ver detalles")
+    opcion1 = int(input("\nElija una opcion: "))
+    
+    if opcion1 == 1:
+        ##Aqui va el codigo para agregar
+    if opcion1 == 2:
+        ##Aqui va el codigo para borrar
+    if opcion1 == 3:
+        ##Aqui va el coigo para modificar
+    if opcion1 == 4:
+        ##Aqui va el codigo para consultar
+    if opcion1 == 5:
+        ##Aqui va el codigo para ver detalles
 
 if opcionInicial == 2:
     print("\n***** CURSOS *****")
     print("1.- Agregar\n2.- Borrar\n3.- Modificar\n4.- Consultar todo\n5.- Ver detalles")
+    opcion2 = int(input("\nElija una opcion: "))
+
+    if opcion2 == 1:
+        ##Aqui va el codigo para agregar
+    if opcion2 == 2:
+        ##Aqui va el codigo para borrar
+    if opcion2 == 3:
+        ##Aqui va el coigo para modificar
+    if opcion2 == 4:
+        ##Aqui va el codigo para consultar
+    if opcion2 == 5:
+        ##Aqui va el codigo para ver detalles
 
 if opcionInicial == 3:
     print("\n***** TEMAS *****")
     print("1.- Agregar\n2.- Borrar\n3.- Modificar\n4.- Consultar todo\n5.- Ver detalles")
+    opcion3 = int(input("\nElija una opcion: "))
+
+    if opcion3 == 1:
+        ##Aqui va el codigo para agregar
+    if opcion3 == 2:
+        ##Aqui va el codigo para borrar
+    if opcion3 == 3:
+        ##Aqui va el coigo para modificar
+    if opcion3 == 4:
+        ##Aqui va el codigo para consultar
+    if opcion3 == 5:
+        ##Aqui va el codigo para ver detalles
 
 if opcionInicial == 4:
     print("\n***** VIDEOS *****")
     print("1.- Agregar\n2.- Borrar\n3.- Modificar\n4.- Consultar todo\n5.- Ver detalles")
+    opcion4 = int(input("\nElija una opcion: "))
+
+    if opcion4 == 1:
+        ##Aqui va el codigo para agregar
+    if opcion4 == 2:
+        ##Aqui va el codigo para borrar
+    if opcion4 == 3:
+        ##Aqui va el coigo para modificar
+    if opcion4 == 4:
+        ##Aqui va el codigo para consultar
+    if opcion4 == 5:
+        ##Aqui va el codigo para ver detalles
 
 if opcionInicial == 5:
     print("\n***** TEMAS ASIGNADOS AL CURSO *****")
     print("1.- Agregar\n2.- Borrar\n3.- Modificar\n4.- Consultar todo\n5.- Ver detalles")
+    opcion5 = int(input("\nElija una opcion: "))
+
+    if opcion5 == 1:
+        ##Aqui va el codigo para agregar
+    if opcion5 == 2:
+        ##Aqui va el codigo para borrar
+    if opcion5 == 3:
+        ##Aqui va el coigo para modificar
+    if opcion5 == 4:
+        ##Aqui va el codigo para consultar
+    if opcion5 == 5:
+        ##Aqui va el codigo para ver detalles
 
 if opcionInicial == 6:
     print("\n***** VIDEOS ASIGNADOS A UN TEMA *****")
     print("1.- Agregar\n2.- Borrar\n3.- Modificar\n4.- Consultar todo\n5.- Ver detalles")
+    opcion6 = int(input("\nElija una opcion: "))
+
+    if opcion6 == 1:
+        ##Aqui va el codigo para agregar
+    if opcion6 == 2:
+        ##Aqui va el codigo para borrar
+    if opcion6 == 3:
+        ##Aqui va el coigo para modificar
+    if opcion6 == 4:
+        ##Aqui va el codigo para consultar
+    if opcion6 == 5:
+        ##Aqui va el codigo para ver detalles
