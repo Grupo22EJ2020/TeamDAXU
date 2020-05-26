58ede06d61864d2a7afa22bd30e7d9270371c724
Ximena Martitínez Hernández <ximenamthdz@gmail.com>
2020-05-26 18:22:47 -0500

    Corrección

--- Temas.py
+++ Temas.py
@@ -16,13 +16,13 @@ def nombre(self):
     return self.nombre
 
 @nombre.setter
-    archivo.close()
+def nombre(self, valor):
+    self.__nombre = valor
+
 
 def agregar(self):
     archivo=open("./archivos/Tema.txt", 'a')
-    idTema=(input("Ingresa el id del tema: n/")) 
-    nombre=(input("Ingresa el nombre del tema: n/"))
-    archivo=write(f"{self.idTema} / {self.nombre} \n")
+    archivo.write(f"{self.idTema} / {self.nombre} \n")
     archivo.close()
 
 
