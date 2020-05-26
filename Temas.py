a5ad876ac530edefc1ef0af78eaf5aa537535d44
Ximena Martitínez Hernández <ximenamthdz@gmail.com>
2020-05-26 00:57:30 -0500

    DAgregué mi clase Tema e hice las propiedades

---
+++ Temas.py
@@ -0,0 +1,18 @@
+class Tema:
+    def __init__(self,idTema,nombre):
+        self.idTema = idTema
+        self.nombre = nombre
+
+@property 
+def idTema(self):
+    return self.idTema
+
+@idTema.setter
+def idTema(self, valor):
+    self.__idTema = valor
+
+@property
+def nombre(self):
+    return self.nombre
+    
+
